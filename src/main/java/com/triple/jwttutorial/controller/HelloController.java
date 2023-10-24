7e88ee1fd4ef6117f28bdc227fc844b76ef757ab
LEYN <leyn53.triple@gmail.com>
2023-10-24 23:43:11 +0900

    Security 설정, Data 설정

--- src/main/java/com/triple/jwttutorial/controller/HelloController.java
+++ src/main/java/com/triple/jwttutorial/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.triple.jwttutorial.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+  @GetMapping("/hello")
   public ResponseEntity<String> hello(){
     return ResponseEntity.ok("hello");
   }
